d8a5726fff29fe7d9dd2994cb53840a6082b51ad
Nicola Stack <nicolastack16@gmail.com>
2024-08-24 11:07:08 +0100

    remove unused imports

--- moviesApp/src/pages/addFantasyMoviePage.tsx
+++ moviesApp/src/pages/addFantasyMoviePage.tsx
@@ -1,22 +1,12 @@
 import React from "react";
-import PageTemplate from "../components/templateMoviePage";
 import MovieForm from "../components/addFantasyMovieForm";
-import { useLocation } from "react-router-dom";
-import { useQuery } from "react-query";
-import { getMovie } from "../api/tmdb-api";
-import Spinner from "../components/spinner";
-import { BaseMovieProps, MovieDetailsProps } from "../types/interfaces";
 
 const CreateFantasyMoviePage: React.FC = () => {
-
-
-    return (
-        <>
-                  
-                        <MovieForm />
-
-        </>
-    );
+  return (
+    <>
+      <MovieForm />
+    </>
+  );
 };
 
 export default CreateFantasyMoviePage;
